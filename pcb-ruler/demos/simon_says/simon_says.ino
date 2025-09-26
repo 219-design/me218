d5480d45156fe8a056747e0521de859d1eb606af
mwong219 <mwong@219design.com>
2025-09-26 09:32:42 -0700

    removed wait for serial connect on setup start

--- pcb-ruler/demos/simon_says/simon_says.ino
+++ pcb-ruler/demos/simon_says/simon_says.ino
@@ -271,9 +271,7 @@ void setup() {
   pinMode(AUDIO_PIN, OUTPUT);
 
   Serial.begin(115200);
-  while (!Serial) {
-    ;  // wait for serial port to connect. Needed for native USB port only
-  }
+
   Serial.println("HELLO");
 
   // Initialize Display
