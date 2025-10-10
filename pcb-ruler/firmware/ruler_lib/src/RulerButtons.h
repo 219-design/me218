2f277c132fa742e8e1190c6dc5f9ebac02d59a6c
mwong219 <mwong@219design.com>
2025-10-10 12:27:02 -0700

    updated comments in RulerButtons.h

--- pcb-ruler/firmware/ruler_lib/src/RulerButtons.h
+++ pcb-ruler/firmware/ruler_lib/src/RulerButtons.h
@@ -22,10 +22,15 @@ class Buttons {
 public:
   Buttons(int center_btn_pin, int left_btn_pin, int right_btn_pin, int up_btn_pin, int down_btn_pin);
 
-  //returns a set of debounce button states indicated by masks
+  /**
+   * gets a set of button presses that have been debounced
+   * needs to be called regularly to debounce the button inputs
+   * @return - each bit of the return value is associated with the BUTTON's MASK.
+   * An asserted bit (1) indicates the button is pressed, a desserted bit (0) indicates the button is not pressed
+   *
   uint32_t get_presses();
 private:
-  const unsigned long c_debounceDelay = 75;  // the debounce time; increase if the output flickers
+  const unsigned long c_debounceDelay = 50;  // the debounce time in ms
   int m_btn_pin[NUM_BTNS] = { INVALID_PIN, INVALID_PIN, INVALID_PIN, INVALID_PIN, INVALID_PIN };
   uint8_t m_last_btn_rd[NUM_BTNS] = { BTN_DEASSERT_LEVEL, BTN_DEASSERT_LEVEL, BTN_DEASSERT_LEVEL, BTN_DEASSERT_LEVEL, BTN_DEASSERT_LEVEL };  //raw read
   uint8_t m_last_btn_debounce_time[NUM_BTNS] = { 0 };
